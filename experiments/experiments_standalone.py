c39f35c23fdf3976eb11d9f5486e734b3a10bedb
Amir Khaki <amirkhaki995@gmail.com>
2026-07-01 12:10:08 +0330

    fix: correct model import paths in experiments_standalone.py

--- experiments/experiments_standalone.py
+++ experiments/experiments_standalone.py
@@ -14,8 +14,8 @@ from .dataset import TO_TENSOR, get_dataset
 from .utils import out_path, save_json, already_done, compute_metrics
 from .helpers import run_slug, run_config
 from .registry import ExperimentBase, register_experiment
-from .baseline import _build_baseline_model
-from .patch import _build_patch_model
+from .models.baseline import _build_baseline_model
+from .models.patch import _build_patch_model
 
 
 @register_experiment
