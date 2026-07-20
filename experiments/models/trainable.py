1dee129a210c944b4ba6ab9933c58c81a38306f3
Amir Khaki <amirkhaki995@gmail.com>
2026-07-20 18:59:44 +0330

    Add BatchNorm1d to regression head to stabilize Gram matrix values

--- experiments/models/trainable.py
+++ experiments/models/trainable.py
@@ -18,6 +18,7 @@ class TrainableRegressionHead(nn.Module):
     def __init__(self, in_features, hidden_dim=256):
         super().__init__()
         self.fc = nn.Sequential(
+            nn.BatchNorm1d(in_features),
             nn.Linear(in_features, hidden_dim),
             nn.ReLU(),
             nn.Dropout(0.5),
